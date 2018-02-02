3eb7430f612817e0a4570d96340cd5baec210961
Daniel Sifton <Daniel.Sifton@viu.ca>
2018-02-02 11:02:56 -0800

    mod susnspot

--- supplejack_api_template.rb
+++ supplejack_api_template.rb
@@ -62,17 +62,18 @@ if pids.present?
 end
 
 rake 'sunspot:solr:start'
-solr_resposne = ''
+#solr_resposne = ''
 
-while solr_resposne.blank?
-  puts 'Sleeping until sunspot is responsive...'
-  sleep 2
-  solr_resposne = `curl -IL --silent http://127.0.0.1:8982 | grep '200 OK'`
-  puts solr_resposne
-end
+#while solr_resposne.blank?
+#  puts 'Sleeping until sunspot is responsive...'
+#  sleep 2
+#  solr_resposne = `curl -IL --silent http://127.0.0.1:8982 | grep '200 OK'`
+#  puts solr_resposne
+#end
 
-puts 'Solr has started.'
-
+#puts 'Solr has started.'
+-puts '\nSleeping for 15 seconds because sunspot is can be slow to wake up in the morning... \n'
+-sleep 15
 
 # ------------------------------------------------------
 # Start Sidekiq
